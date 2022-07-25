4c38ca79b62d2b4447d1d06ea2f994fa04913604
Oivlisfriend <robertomarcelogongo@gmail.com>
2022-07-25 02:23:34 +0100

    TextInput obrigatória

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -43,6 +43,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackSent, onFeedbackRe
             </header>
             <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea
+                    required
                     className="min-w[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:outline-none focus:ring-brand-500 focus:ring-1 reasize:none scrollbar-thumb-zinc-700 scrollbar scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
                     onChange={event => setComment(event.target.value)}
